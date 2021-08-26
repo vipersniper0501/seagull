76d1694f4c06e4242b5cfcb00e4542b39bfb40c8
ViperSniper0501 <michaelabrenner@gmail.com>
2021-08-26 21:00:11 +0200

    Can now use multiple lights

--- seagull/tmp_shaders/defaultFragment.glsl
+++ seagull/tmp_shaders/defaultFragment.glsl
@@ -1,32 +1,12 @@
 #version 400
 
 
+#define MAX_NUMBER_LIGHTS 32
 // in vec2 TexCoords;
 // in vec3 FragPos;
 // in vec3 Normal;
 
-// in mat3 TBN;
-
-in VS_OUT {
-    vec3 FragPos;
-    vec2 TexCoords;
-    vec3 TangentLightPos;
-    vec3 TangentViewPos;
-    vec3 TangentFragPos;
-} fs_in;
-
-out vec4 FragColor;
-
-
-struct Material {
-    sampler2D texture_diffuse1;
-    sampler2D texture_specular1;
-    sampler2D texture_normal1;
-    sampler2D texture_height1;
-    sampler2D texture_ao1;
-    sampler2D texture_emissive1;
-    float shininess;
-};
+in mat3 TBN;
 
 struct Light {
     vec3 position;
@@ -42,30 +22,40 @@ struct Light {
     float quadratic;
 };
 
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    // vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+    // Light Lights[4];
+} fs_in;
 
-uniform Light lighting;
+out vec4 FragColor;
+
+
+struct Material {
+    sampler2D texture_diffuse1;
+    sampler2D texture_specular1;
+    sampler2D texture_normal1;
+    sampler2D texture_height1;
+    sampler2D texture_ao1;
+    sampler2D texture_emissive1;
+    float shininess;
+};
+
+
+uniform vec3 lightPos[64];
+Light Lights[MAX_NUMBER_LIGHTS];
+uniform Light lighting[64];
+uniform int nrLights;
 
 uniform Material material;
 
 
-// vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
-// {
-    // vec3 lightDir = normalize(-light.direction);
-    // // diffuse shading
-    // float diff = max(dot(norm, lightDir), 0.0);
-    // // specular shading
-    // vec3 reflectDir = reflect(-lightDir, normal);
-    // float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    // // combine results
-    // vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
-    // vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
-    // vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
-    // return (ambient + diffuse + specular);
-// }
-
-vec3 CalcPointLight(Light light, vec3 normal, vec3 fragPos, vec3 viewDir)
+vec3 CalcPointLight(Light light, vec3 normal, vec3 fragPos, vec3 viewDir, int lightIndex)
 {
-    vec3 lightDir = normalize(fs_in.TangentLightPos - fragPos);
+    vec3 lightDir = normalize(light.position - fragPos);
     vec3 halfwayDir = normalize(lightDir + viewDir);
     // diffuse shading
     float diff = max(dot(normal, lightDir), 0.0);
@@ -80,7 +70,7 @@ vec3 CalcPointLight(Light light, vec3 normal, vec3 fragPos, vec3 viewDir)
     vec3 emissive = texture(material.texture_emissive1, fs_in.TexCoords).rgb;
 
     // attenuation
-    float distance    = length(fs_in.TangentLightPos - fragPos);
+    float distance    = length(light.position - fragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + 
   			     light.quadratic * (distance * distance));    
     // combine results
@@ -105,7 +95,23 @@ void main(void)
     vec3 ao = texture(material.texture_ao1, fs_in.TexCoords).rgb;
     vec3 emissive = texture(material.texture_emissive1, fs_in.TexCoords).rgb;
     vec3 diff = texture(material.texture_diffuse1, fs_in.TexCoords).rgb;
+    vec3 spec = texture(material.texture_specular1, fs_in.TexCoords).rgb;
 
-    vec3 result = CalcPointLight(lighting, norm, fs_in.TangentFragPos, viewDir);
+
+    for (int i = 0; i < nrLights; i++)
+    {
+        // assigning light position with calculated Tangent Light Position
+        Lights[i] = lighting[i];
+        Lights[i].position = TBN * lightPos[i];
+    }
+
+
+
+    vec3 result;
+
+    for(int i = 0; i < nrLights; i++)
+    {
+        result += CalcPointLight(Lights[i], norm, fs_in.TangentFragPos, viewDir, i);
+    }
     FragColor = vec4(result, 1.0);
 }
--- seagull/tmp_shaders/defaultVertex.glsl
+++ seagull/tmp_shaders/defaultVertex.glsl
@@ -6,29 +6,55 @@ layout(location=2) in vec2 in_TexCoords;
 layout(location=3) in vec3 in_Tangents;
 layout(location=4) in vec3 in_Bitangents;
 
+#define MAX_NUMBER_LIGHTS 32
+
+struct Light {
+    vec3 position;
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    // attenuation
+    float constant;
+    float linear;
+    float quadratic;
+};
+
 out VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
-    vec3 TangentLightPos;
+    // vec3 TangentLightPos;
     vec3 TangentViewPos;
     vec3 TangentFragPos;
+    // Light Lights[4];
 } vs_out;
 
 // out vec2 TexCoords;
 // out vec3 Normal;
 // out vec3 FragPos;
 
-// out mat3 TBN;
+out mat3 TBN;
+
+
+// uniform Light lighting[64];
+// uniform int nrLights;
 
 uniform mat4 ModelMatrix;
 uniform mat4 ViewMatrix;
 uniform mat4 ProjectionMatrix;
 
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 void main(void)
 {
+
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     vs_out.Lights[i] = lighting[i];
+    // }
+
     vs_out.FragPos = vec3(ModelMatrix * vec4(in_Position, 1.0));
     vs_out.TexCoords = in_TexCoords;
 
@@ -40,8 +66,13 @@ void main(void)
     T = normalize(T - dot(T, N) * N);
     vec3 B = cross(N, T);
 
-    mat3 TBN = transpose(mat3(T, B, N));
-    vs_out.TangentLightPos = TBN * lightPos;
+    TBN = transpose(mat3(T, B, N));
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     // vs_out.TangentLightPos = TBN * lightPos[i];
+    //     // assigning light position with calculated Tangent Light Position
+    //     vs_out.Lights[i].position = TBN * lightPos[i];
+    // }
     vs_out.TangentViewPos  = TBN * viewPos;
     vs_out.TangentFragPos  = TBN * vs_out.FragPos;
 
