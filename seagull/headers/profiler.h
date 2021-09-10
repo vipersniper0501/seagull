bcd7e2b6b48dd252f911007f88446cce73dd6e59
ViperSniper0501 <michaelabrenner@gmail.com>
2021-09-10 17:29:54 +0200

    Updated profiler.h

--- seagull/headers/profiler.h
+++ seagull/headers/profiler.h
@@ -1,23 +1,26 @@
 #ifndef PROFILER_HEADER_FILE
 #define PROFILER_HEADER_FILE
 
-#include "optick.h"
 
 // I use the Optick Profiler for debugging long frames
 #ifndef __MINGW32__
-    #ifndef NDEBUG 
-        #define SGL_PROFILE_MAIN() OPTICK_FRAME("MainThread");
-        #define SGL_PROFILE_FUNCTION() OPTICK_EVENT();
-        #define SGL_PROF_THEAD(NAME) OPTICK_THREAD(NAME);
-    #else
-        #define SGL_PROFILE_MAIN();
-        #define SGL_PROFILE_FUNCTION();
-        #define SGL_PROF_THEAD(NAME);
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+        #include "optick.h"
+        #ifndef NDEBUG // yes debugging
+            #define SGL_PROFILE_MAIN() OPTICK_FRAME("MainThread")
+            #define SGL_PROFILE_FUNCTION() OPTICK_EVENT()
+            #define SGL_PROF_THEAD(NAME) OPTICK_THREAD(NAME)
+        #else
+            #define SGL_PROFILE_MAIN() do {} while(0);
+            #define SGL_PROFILE_FUNCTION() do {} while(0);
+            #define SGL_PROF_THEAD(NAME) do {} while(0);
+        #endif
     #endif
-#else // Don't use these if using mingw
-    #define SGL_PROFILE_MAIN();
-    #define SGL_PROFILE_FUNCTION();
-    #define SGL_PROF_THEAD(NAME);
+#else
+    #define SGL_PROFILE_MAIN() do {} while(0);
+    #define SGL_PROFILE_FUNCTION() do {} while(0);
+    #define SGL_PROF_THEAD(NAME) do {} while(0);
 #endif
+
 
 #endif
